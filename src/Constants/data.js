203354653e38d5894c6e7ac6dc8cd8b06f3ed691
Nino <adavaoluwatobisamuel@gmail.com>
2023-07-03 14:00:31 +0100

    blog post roughly done

---
+++ src/Constants/data.js
@@ -0,0 +1,20 @@
+import { meal, random, second, third } from './index';
+
+const data = [
+  {
+    duration: '30s',
+    imag: random
+  },
+
+  {
+    duration: '45s',
+    imag: second
+
+  },
+  {
+    duration: '40s',
+    imag: third
+  }
+];
+
+export { data };
