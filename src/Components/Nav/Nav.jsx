a4a5764eb813b867a6c280f4caf8baa7beacd11c
Nino <adavaoluwatobisamuel@gmail.com>
2023-07-13 05:13:39 +0100

    the hamburger menu changed and updated

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -6,6 +6,9 @@ import Login from "../Login";
 import Signup from "../Signup";
 import { BiMenuAltRight } from "react-icons/bi";
 import { FaLinesLeaning } from "react-icons/fa6";
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md'
+import OutsideClickHandler from 'react-outside-click-handler'
 
 const style = {
   maxHeight: "800px",
@@ -23,6 +26,7 @@ const style = {
 };
 
 function Nav() {
+  const [toggleMenu, setToggleMenu] = useState(false)
   const [open, setOpen] = useState(false);
   const [authType, setAuthType] = useState();
 
@@ -31,13 +35,7 @@ function Nav() {
     setAuthType(e.target.id);
   };
   const handleClose = () => setOpen(false);
-  const [menuOpened, setMenuOpened] = useState(false);
 
-  const getMenuStyles = (menuOpened) => {
-    if (document.documentElement.clientWidth <= 800) {
-      return { right: !menuOpened ? "0" : "-100%" };
-    }
-  };
 
   return (
     <div>
@@ -46,8 +44,7 @@ function Nav() {
           <h3>SCOUTING REPORT</h3>
         </div>
         <div
-          className={`header-list flexStart ${menuOpened ? "active" : ""}`}
-          style={getMenuStyles(menuOpened)}
+          className={`header-list flexStart`}
         >
           <Link to="/">
             {" "}
@@ -75,12 +72,58 @@ function Nav() {
           </div>
         </div>
 
-        <div
-          className="menu-icon"
-          onClick={() => setMenuOpened((prev) => !prev)}
-        >
-          <BiMenuAltRight size={30} />
-        </div>
+        {/*  */}
+        
+
+        <div className='app__navbar-smallscreen'>
+      <GiHamburgerMenu color='#fff' className="hamburger" fontSize={25} onClick={() => setToggleMenu(true)}/>
+
+
+    {toggleMenu && (
+    <div className='app__navbar-smallscreen_overlay '>
+
+      <OutsideClickHandler onOutsideClick={() => setToggleMenu(false)}>
+      
+      <ul className='app__navbar-smallscreen-links'>
+      <Link to="/">
+            {" "}
+            <div>HOME</div>{" "}
+          </Link>
+          <Link to="/discover">
+            {" "}
+            <div>DISCOVER</div>{" "}
+          </Link>
+          <Link to="/blog">
+            {" "}
+            <div>BLOG</div>{" "}
+          </Link>
+          <div
+            id="login"
+            onClick={(e) => handleOpen(e)}
+            style={{ cursor: "pointer" }}
+          >
+            LOGIN
+          </div>
+          <div className="header-btn">
+            <button id="signup" onClick={(e) => handleOpen(e)}>
+              REGISTER
+            </button>
+          </div>
+      
+    </ul>
+
+      </OutsideClickHandler>
+      
+
+    </div>
+    )}
+
+
+     
+
+    </div>
+
+    
       </div>
 
       <Modal open={open} onClose={handleClose}>
